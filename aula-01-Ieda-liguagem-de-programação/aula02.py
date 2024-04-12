93743424e02bbbd68f65567541d7672378eb764c
fabimaria <iedasouzalm@gmail.com>
2024-04-11 21:50:47 -0300

    update python
    Parabéns Gizelle

---
+++ aula-01-Ieda-liguagem-de-programação/aula02.py
@@ -0,0 +1,12 @@
+#tudo que tiver entre parnteses vai exibir na tela
+#print("finalmente quebrei a maldição")  
+
+#quando eu faço um comenterio simples
+##um bloco de comentario
+
+print("Olá, bem vindo ao nosso app!")
+#print("")
+nome = input("Qual é o seu nome ?:")
+idade = input("Qual é sua idade ?:")
+gosto = input("Qual é o seu hobbie ?:")
+print(f"Olá {nome}, você tem {idade} anos, seu hobbie é {gosto}")
